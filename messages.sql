72e20424071c4565369b9ec41b813b15bae21b56
Delyan Raychev <delyan@raychev.org>
2016-09-03 11:37:31 -0700

    Lets use lower-case only column names in PSQL

--- messages.sql
+++ messages.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
        id VARCHAR(256) PRIMARY KEY,
        ts TIMESTAMP,
-       rowID INT,
-       myCall VARCHAR(8),
+       rowid INT,
+       mycall VARCHAR(8),
        rpt1 VARCHAR(8),
        rpt2 VARCHAR(8),
-       urCall VARCHAR(8),
-       qsoStarted BOOLEAN,
+       urcall VARCHAR(8),
+       qsostarted BOOLEAN,
        flags VARCHAR(6),
-       myRadio VARCHAR(4),
+       myradio VARCHAR(4),
        dest VARCHAR(8),
-       txStatus VARCHAR(20)
+       txstats VARCHAR(20)
 );
